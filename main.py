6624b4012f106d7c5cb861cd88c7837e3edf01a5
devey <devey@devey-ubuntu.(none)>
2024-02-07 11:42:30 +0530

    testcommit1

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+Fast Propagation of EM Waves in Graded Index Media (GRIN)
+Mini-Project; MSc Program, IIT Delhi
+Devey Singh Rathore (2022PHS7194)
+Superviser: Prof. Anurag Sharma
+
+Goals:
+1) Recreate the plots of Zhong. et. al (https://doi.org/10.1364/JOSAA.35.000661) 
+2) Make a package that's "proper"
+''' 
+
+# Define index array
+# Nx * Ny * Nz dimensions for the sample
+# [[nx1, ny1, nz1], [nx2, ny2, nz2], ..., [nxNx, nyNy, nzNz]]
+# or N(r) is a function that's given.
+
+
+# Define initial field
+# E = [[Ex, Ey, Ez], ... [Ex, Ey, Ez]]
+
+
+# Sample geometry: d long [stick to SI units i.e meters]
+# 
+
+# test push 1
+# still test 1
